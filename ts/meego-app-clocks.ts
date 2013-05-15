1fda1636be1e7254bcf87e82b94dd2713db2c28c
Victor Dudochkin <dudochkin.victor@gmail.com>
2013-05-15 14:42:52 +0800

    Initial commit

---
+++ ts/meego-app-clocks.ts
@@ -0,0 +1,210 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Alarm</name>
+    <message>
+        <location filename="../../Alarm.qml" line="31"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Alarm.qml" line="31"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewAlarmMenu</name>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="24"/>
+        <source>Mo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="25"/>
+        <source>Tu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="26"/>
+        <source>We</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="27"/>
+        <source>Th</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="28"/>
+        <source>Fr</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="29"/>
+        <source>Sa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="30"/>
+        <source>Su</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="32"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="32"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="33"/>
+        <source>sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="33"/>
+        <source>track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="351"/>
+        <source>Snooze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="389"/>
+        <location filename="../../NewAlarmMenu.qml" line="437"/>
+        <source>1 Minute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="389"/>
+        <location filename="../../NewAlarmMenu.qml" line="437"/>
+        <source>%1 Minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="509"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="664"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="883"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewAlarmMenu.qml" line="902"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewClockMenu</name>
+    <message>
+        <location filename="../../NewClockMenu.qml" line="113"/>
+        <source>(GMT %1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewClockMenu.qml" line="113"/>
+        <source>(GMT +%1)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewClockMenu.qml" line="154"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewClockMenu.qml" line="172"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../main.qml" line="18"/>
+        <location filename="../../main.qml" line="28"/>
+        <location filename="../../main.qml" line="137"/>
+        <source>Clocks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="18"/>
+        <location filename="../../main.qml" line="34"/>
+        <source>Alarms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="59"/>
+        <source>yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="60"/>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="61"/>
+        <source>Are you sure you want to delete?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="144"/>
+        <source>New alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="144"/>
+        <source>New clock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="374"/>
+        <source>Alarm details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="374"/>
+        <source>Delete alarm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="374"/>
+        <location filename="../../main.qml" line="392"/>
+        <source>Move to top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="383"/>
+        <location filename="../../main.qml" line="392"/>
+        <source>Edit clock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="383"/>
+        <location filename="../../main.qml" line="392"/>
+        <source>Make local time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../main.qml" line="383"/>
+        <location filename="../../main.qml" line="392"/>
+        <source>Delete clock</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
